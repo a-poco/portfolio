e2686d4a6e09fe06539c6a5f81e9aba5a0829870
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-05-31 11:54:49 +0200

    add gif to projects and style

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,17 +4,22 @@ const Projects = () => {
   return (
     <div className='projects'>
       <header className='projects__tittle'>My Projects</header>
-      <article className='projects__info'>
-        <h1  className='projects-info__name'>ParkN</h1>
-        <p  className='projects-info__description'>
-        eam Tasks is a task list application created with React, TypeScript and Firestore. <br/>
-        Feature include basic CRUD functionality, drag and drop, livesharing, authentication and offline persistence.<br/>
-        React, TypeScript, Firestore, Styled-Components, React Beautful DnD, HTML, CSS, JavaScript
-        </p>
-        <span  className='project-info__btns'>
-          <button className='project-info__btn'>Live Demo</button>
-          <button className='project-info__btn'>GitHub Link</button>
-        </span>
+      <article className='projects__block'>
+        <div className='projects-block__info'>
+          <h1  className='projects-info__name'>ParkN</h1>
+          <p  className='projects-info__description'>
+          eam Tasks is a task list application created with React, TypeScript and Firestore. <br/>
+          Feature include basic CRUD functionality, drag and drop, livesharing, authentication and offline persistence.<br/>
+          React, TypeScript, Firestore, Styled-Components, React Beautful DnD, HTML, CSS, JavaScript
+          </p>
+          <span  className='project-info__btns'>
+            <button className='project-info__btn'>Live Demo</button>
+            <button className='project-info__btn'>GitHub Link</button>
+          </span>
+        </div>
+        <div className='project-info__img'>
+          <img className='parkn' src="parkn.gif" alt='Cristina'/>
+        </div>
       </article>
     </div>
   )
