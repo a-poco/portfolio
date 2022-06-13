d8426645e6f2c69e92e0d654791088dd0e20043e
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-06-13 14:58:18 +0200

    adding backgound color to nav icones

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import { BsGithub, BsLinkedin, BsFillEnvelopeFill } from "react-icons/bs";
 import './Navbar.css'
 
+const style = { backgroundColor: "white"}
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <h1 className='navbar__title'>Cristina Salazar</h1>
       <section className='navbar__icones'>
-        <a href={"https://github.com/a-poco"} target={"_blank" } className='navbar__icone' rel="noreferrer"><BsGithub /></a>
-        <a  href={"https://www.linkedin.com/in/cristinasalazarmunguia/"} target={"_blank" } className='navbar__icone' rel="noreferrer"><BsLinkedin/></a>
-        <a  href={"mailto:cristina.munguia@appliedtechnology.se?subject=Hey Cristina!"} target={"_blank" } rel="noreferrer" className='navbar__icone'><BsFillEnvelopeFill/></a>
+        <a href={"https://github.com/a-poco"} target={"_blank" } className='navbar__icone' rel="noreferrer"><BsGithub style={style} /></a>
+        <a  href={"https://www.linkedin.com/in/cristinasalazarmunguia/"} target={"_blank" } className='navbar__icone' rel="noreferrer"><BsLinkedin style={style} /></a>
+        <a  href={"mailto:cristina.munguia@appliedtechnology.se?subject=Hey Cristina!"} target={"_blank" } rel="noreferrer" className='navbar__icone'><BsFillEnvelopeFill style={style} /></a>
       </section>
     </nav>
   ) 
