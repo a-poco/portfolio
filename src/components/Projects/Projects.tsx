b1a513c4843a108180759ce78f92bd8dd79226c5
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-06-13 15:29:59 +0200

    fix typo

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -28,7 +28,7 @@ const Projects = () => {
           <p  className='projects-info__description'>
           A weather app where you can find the weather in any city! 
           Feching the information of the weather from an expertal api.
-          Express.Js, Node.Js, JavaScript, React, TypeScript, HTML, CSS, 
+          Express.Js, Node.Js, JavaScript, React, TypeScript, HTML, CSS. 
           </p>
           <span  className='project-info__btns'>
             <a href={"https://www.parkn.club/"} target={"_blank" } className='project-info__btn' rel="noreferrer">Live Demo</a>
@@ -45,7 +45,7 @@ const Projects = () => {
           <p  className='projects-info__description'>
             Find the perfect macth for a play date to your dog!
             Pet me Up was created as a graduaction project for Salt JavaScript course.
-            Mongodb, Node.js, Express.js, React, Redux, Typescript, Firebase, Geocoding, HTML, CSS,
+            Mongodb, Node.js, Express.js, React, Redux, Typescript, Firebase, Geocoding, HTML, CSS.
           </p>
           <span  className='project-info__btns'>
             <a href={"https://ancient-basin-65065.herokuapp.com/"} target={"_blank" } className='project-info__btn' rel="noreferrer">Live Demo</a>
@@ -63,7 +63,8 @@ const Projects = () => {
             My personal portfolio page built with React 
             which is used as a platform to display my work.
             The application is fully responsive. 
-            React, TypeScript, HTML, CSS,
+            React, TypeScript, HTML, CSS.
+            Deploy on Netlify.
           </p>
           <span  className='project-info__btns'>
           <a href={"https://github.com/a-poco/portfolio"} target={"_blank" } className='project-info__btn' rel="noreferrer">GitHub Link</a>
